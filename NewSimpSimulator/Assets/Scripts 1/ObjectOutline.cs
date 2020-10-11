76dd0cba1c67bfa1be2d2a3f1709282525d89c24
Nick Buscemi <nickbuscemi207@gmail.com>
2020-10-11 17:50:10 -0400

    Please

---
+++ NewSimpSimulator/Assets/Scripts 1/ObjectOutline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOutline : MonoBehaviour
+{
+
+    private MeshRenderer renderer;
+    public float maxOutlineWidth;
+    public Color outlineColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    public void ShowOutline()
+    {
+        renderer.material.SetFloat("_Outline", maxOutlineWidth);
+        renderer.material.SetColor("_OultineColor", outlineColor);
+    }
+
+    public void HideOutline()
+    {
+        renderer.material.SetFloat("_Outline", 0f);
+    }
+
+}
